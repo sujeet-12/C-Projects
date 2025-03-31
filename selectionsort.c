eb869c4d1b73b2bf88270d13896cdfc37e6244c2
Hritik Gautam <85284353+sujeet-12@users.noreply.github.com>
2025-04-01 01:12:18 +0530

    Create selectionsort.c

---
+++ selectionsort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted Array: ");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Sorted Array: ");
+    printArray(arr, n);
+    return 0;
+}
